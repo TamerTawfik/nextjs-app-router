b7d40741c9e457973299643b62a14779816cd679
Tamer Tawfik <tamertawfiklxr@gmail.com>
2023-05-15 02:14:54 +0200

    Added: Packages and Assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,4 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import '../styles/globals.css'
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +12,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+        <main className='app'>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
